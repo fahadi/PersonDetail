ac43305d776ff90c2e0580b2593259264025cc40
fahadi <fahadifthkar@gmail.com>
2019-12-28 22:58:36 +0000

    Created a Personal wsdl from scratch, experimenting with jax-ws and spring-ws.

---
+++ src/main/java/com/fahadi/PersonDetail/PersonDetailApplication.java
@@ -0,0 +1,13 @@
+package com.fahadi.PersonDetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonDetailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fahadi/PersonDetail/PersonDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahadi.PersonDetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonDetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
