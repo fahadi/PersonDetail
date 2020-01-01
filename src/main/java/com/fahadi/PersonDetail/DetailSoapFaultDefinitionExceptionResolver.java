1ae05ac8da60cf97977d7b8ca7bf72fe299d52d0
fahadi <fahadifthkar@gmail.com>
2020-01-01 22:56:27 +0000

    Created a DetailSoapFaultDefinitionExceptionResolver, this allows for exceptions objects to be transformed into xml and sent to in the soap message.

--- src/main/java/com/fahadi/PersonDetail/DetailSoapFaultDefinitionExceptionResolver.java
+++ src/main/java/com/fahadi/PersonDetail/DetailSoapFaultDefinitionExceptionResolver.java
@@ -7,10 +7,14 @@ package com.fahadi.PersonDetail;
 
 import com.fahadi.person.MyFaultDetail;
 import com.fahadi.person.MyFaultDetailMessage;
-import com.fahadi.person.ObjectFactory;
-import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBContext;
-import org.springframework.oxm.Marshaller;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.transform.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import org.springframework.ws.soap.SoapFault;
 import org.springframework.ws.soap.SoapFaultDetail;
 import org.springframework.ws.soap.server.endpoint.SoapFaultMappingExceptionResolver;
@@ -22,12 +26,28 @@ import org.springframework.ws.soap.server.endpoint.SoapFaultMappingExceptionReso
 public class DetailSoapFaultDefinitionExceptionResolver extends 
         SoapFaultMappingExceptionResolver {
     
-     @Override
-    protected void customizeFault(Object endpoint, Exception ex, SoapFault fault) {
-        logger.warn("Exception processed ", ex);
+    private static final Logger logger = LoggerFactory.getLogger(DetailSoapFaultDefinitionExceptionResolver.class);
+    
+    private final Marshaller marshaller;
+    
+    public DetailSoapFaultDefinitionExceptionResolver() throws JAXBException{
+        JAXBContext jAXBContext = JAXBContext.newInstance(MyFaultDetail.class);
+        marshaller = jAXBContext.createMarshaller();
+    }
+    
+    @Override
+    protected void customizeFault(Object endpoint, Exception ex, 
+            SoapFault fault) {
+        logger.warn("Exception processed ", ex.getLocalizedMessage());
         if (ex instanceof MyFaultDetailMessage) {
             MyFaultDetail serviceFault = ((MyFaultDetailMessage) ex).getFaultInfo();
             SoapFaultDetail detail = fault.addFaultDetail();
+            Result result = detail.getResult();
+            try {
+                this.marshaller.marshal(serviceFault,result);
+            } catch (JAXBException ex1) {
+                logger.error(ex1.getMessage());
+            }
         }
     }
     
--- src/main/java/com/fahadi/PersonDetail/WebServiceConfig.java
+++ src/main/java/com/fahadi/PersonDetail/WebServiceConfig.java
@@ -5,13 +5,18 @@
  */
 package com.fahadi.PersonDetail;
 
+import com.fahadi.person.MyFaultDetailMessage;
+import java.util.Properties;
 import javax.servlet.Servlet;
+import javax.xml.bind.JAXBException;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.soap.server.endpoint.SoapFaultDefinition;
+import org.springframework.ws.soap.server.endpoint.SoapFaultMappingExceptionResolver;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
@@ -43,6 +48,22 @@ public class WebServiceConfig {
                 .setWsdl(new ClassPathResource("Person.wsdl"));
 
         return wsdl11Definition;
+    }
+    
+    @Bean
+    public SoapFaultMappingExceptionResolver exceptionResolver() throws JAXBException{
+        SoapFaultMappingExceptionResolver exceptionResolver = new DetailSoapFaultDefinitionExceptionResolver();
+
+        SoapFaultDefinition faultDefinition = new SoapFaultDefinition();
+        faultDefinition.setFaultCode(SoapFaultDefinition.SERVER);
+        exceptionResolver.setDefaultFault(faultDefinition);
+
+        Properties errorMappings = new Properties();
+        errorMappings.setProperty(Exception.class.getName(), SoapFaultDefinition.SERVER.toString());
+        errorMappings.setProperty(MyFaultDetailMessage.class.getName(), SoapFaultDefinition.SERVER.toString());
+        exceptionResolver.setExceptionMappings(errorMappings);
+        exceptionResolver.setOrder(1);
+        return exceptionResolver;
     }
 
 }
