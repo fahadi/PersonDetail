b3c26e68df02eaba58999a3f220ce9bfcead86a0
fahadi <fahadifthkar@gmail.com>
2020-01-03 20:55:53 +0000

    tidying up the MarshalUtil class

--- src/main/java/com/fahadi/util/MarshalUtil.java
+++ src/main/java/com/fahadi/util/MarshalUtil.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.fahadi.util;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -13,18 +14,17 @@ import javax.xml.bind.Marshaller;
  * @author developer
  */
 public class MarshalUtil<T> {
-    
-    private T classT;
-    
-    public MarshalUtil(T classT){
+
+    private final T classT;
+
+    public MarshalUtil(final T classT) {
         this.classT = classT;
     }
-    
-    
-    public Marshaller getMarshaller() throws JAXBException{
-        JAXBContext jAXBContext = JAXBContext.newInstance(this.classT.getClass());
-        Marshaller marshaller = jAXBContext.createMarshaller();
+
+    public Marshaller getMarshaller() throws JAXBException {
+        final JAXBContext jAXBContext = JAXBContext.newInstance(this.classT.getClass());
+        final Marshaller marshaller = jAXBContext.createMarshaller();
         return marshaller;
     }
-    
+
 }
